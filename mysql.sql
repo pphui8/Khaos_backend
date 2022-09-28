b9fb2bf98b253655fecc7e783832d97f36aeb71f
金晖 <55843852+pphui8@users.noreply.github.com>
2022-09-28 10:25:06 +0800

    v1.0.0

--- mysql.sql
+++ mysql.sql
@@ -40,7 +40,71 @@ CREATE TABLE IF NOT EXISTS `product`(
    PRIMARY KEY ( `id` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- check the product.sql
+insert into `product` (productname, descript, price, stock, sale, img, type, status) 
+values('BANPRESTO 龙珠Z 孙悟饭 景品手办', 'BANPRESTO出品手办', 119, 67, 2,'https://cbu01.alicdn.com/img/ibank/2019/904/262/12069262409_1782537758.jpg','Q版手办', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status) 
+values('哔哩哔哩 环球萌动系列 小电视 晴雨伞', '小电视 晴雨伞', 68, 117, 3, 'https://img.alicdn.com/bao/uploaded/i2/2138521987/O1CN01bzjZs61QY5fy2bEmC_!!0-item_pic.jpg_300x300q90.jpg','日居用品', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('哔哩哔哩 全垒打系列 2233 毛巾', 'bilibili毛巾', 25, 211, 0,'https://p1.ssl.qhimgs1.com/sdr/400__/t01ba842aeaebc76bfb.jpg','日居用品', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('BILIBILIGOODS 软萌大礼包', 'bilibili软萌大礼包', 327, 111, 1, 'https://img13.360buyimg.com/n5/s800x800_jfs/t1/111266/4/5765/732846/5eb6456fE01bd1067/68fbf7f6ca6b2204.jpg','日居用品', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('《DRAGON BALL超七龙珠超》1-17平装版 漫画 港台图书', '七龙珠图书', 425, 337, 0,'https://img.alicdn.com/bao/uploaded/i2/4217166954/O1CN01xICI0H21EyzfBOBjE_!!0-item_pic.jpg_310x310.jpg','漫画', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('万代 龙珠Z 弗利萨 终极形态 拼装模型', '万代拼装模型', 109, 71, 2,'https://p5.ssl.qhimgs1.com/sdr/400__/t011251f2df1f82e615.jpg','手办', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('BANPRSETO 龙珠Z 贝吉塔 景品手办', 'BANPRSETO景品手办', 119, 77, 1,'https://r3.hpoi.net.cn/gk/pic/s/2020/07/7f2e0b61c10644e18c4eb6fc1b69f6e6.jpg','手办', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('BANPRSETO 超级赛亚人之神 贝吉塔 景品手办', 'BANPRSETO景品手办', 45, 66, 0, 'https://p0.ssl.qhimgs1.com/sdr/400__/t0138df0d08b86a820a.jpg','手办', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('《七龙珠彩色漫画 魔人布欧篇》1-6 平装版 漫画 港台图书', '七龙珠图书', 300, 118, 2, 'https://img.alicdn.com/bao/uploaded/i2/1793589415/O1CN01lbmtCk2JQ7ZkKXLPV_!!0-item_pic.jpg','漫画', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('《DRAGON BALL 590 QUIZ BOOK七龙珠590解密打全》 平装版 漫画 港台图书', '七龙珠590解密打全图书', 40, 99, 0,'https://mhfm1cnc.cdndm5.com/38/37554/20170718134544_320x246_56.jpg','漫画', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('《七龙珠彩色漫画 少年篇》1-8 平装版漫画 港台图书', '七龙珠漫画', 400, 87, 0, 'https://img.alicdn.com/bao/uploaded/i1/1793589415/O1CN01BmBrhd2JQ7Zm50XRr_!!0-item_pic.jpg','漫画', '停售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('《七龙珠彩色漫画 人造人·赛鲁篇》1-6 平装版漫画 港台图书', '七龙珠漫画', 300, 71, 1, 'http://img3m4.ddimg.cn/62/22/1192492484-1_w_2.jpg','漫画', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('灵动创想 龙珠Z系列 钥匙扣滴胶挂件', '龙珠滴胶挂件', 29, 291, 1,'https://cbu01.alicdn.com/img/ibank/2019/168/243/10984342861_1384209607.jpg','摆件挂件', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('CALORIE 龙珠Z午睡套餐U型枕眼罩', '龙珠Z午睡套餐U型枕眼罩', 139, 44, 1, 'http://5b0988e595225.cdn.sohucs.com/images/20190701/11e74364c69140319191657af8096374.jpeg','日居用品', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('BANPRSETO（眼镜厂）龙珠GT 超级赛亚人之神4 贝吉塔 景品手办', '眼镜厂景品手办', 119, 77, 0, 'http://img.mp.itc.cn/upload/20170328/329c443003724b3a94aa20325eddb400_th.jpg','手办', '停售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('慢慢淘 龙珠系列 OAT恤 周边', '龙珠T恤', 99, 39, 0, 'https://img.alicdn.com/bao/uploaded/i4/2241650131/TB2UBommbJmpuFjSZFwXXaE4VXa_!!2241650131.jpg_300x300.jpg','日居用品', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('万代 S.H.Figuarts龙珠Z萨博 弗利萨军团 魂限定 可动手办', '可动手办', 429, 77, 2, 'http://n.sinaimg.cn/sinakd20100/728/w1170h1158/20211010/b5ff-94e950f9c64b5a8c245251f463babd30.jpg','手办', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('《七龙珠彩色漫画 GT版》1-8 平装版漫画 港台图书', '七龙珠漫画', 300, 21, 3, 'https://img.zcool.cn/community/01dfc25f31f832a801215aa038470c.jpg@2o.jpg','漫画', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('哔哩哔哩 小电视年糕毛绒抱枕 周边', 'bilibili抱枕周边', 159, 61, 3, 'https://gd1.alicdn.com/imgextra/i1/22898759/O1CN013tRADl2EZfuokj9UE_!!0-item_pic.jpg','日居用品', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('bilibili周边 哔哩哔哩小电视双层透明杯子 水杯 创意玻璃杯', 'bilibili周边创意玻璃杯', 80, 77, 1, 'https://img11.360buyimg.com/n5/s800x800_jfs/t1/195580/6/4261/67743/60a73135Efc8929f9/452c2d8f5418d79b.jpg','摆件挂件', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('bilibiliGoods 哔哩哔哩 小电视木质手机支架', 'bilibili手机支架', 38, 83, 2, 'https://img10.360buyimg.com/n5/s800x800_jfs/t1/194238/7/11134/102850/60dd850cE8adf09f3/40ed99f34f4e62df.jpg','摆件挂件', '在售');
+
+insert into `product` (productname, descript, price, stock, sale, img, type, status)
+values('bilibili哔哩哔哩 人生一串 小电视美食盒蛋', 'bilibili美食盒蛋', 59, 70, 3, 'https://r3.hpoi.net.cn/gk/cover/n/2019/09/74e973282d604464a1c07c70dbdc8377.jpg?date=1569403156','摆件挂件', '在售');
 
 -- 订单表
 CREATE TABLE IF NOT EXISTS `orders`(
